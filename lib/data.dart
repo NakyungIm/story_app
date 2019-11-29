bad3cd11bc4024621cc748b39c67a9d093282f99
NakyungIm <nakyoung99@gmail.com>
2019-11-29 21:25:13 +0900

    first commit

---
+++ lib/data.dart
@@ -0,0 +1,13 @@
+List<String> images = [
+  "assets/image_01.png",
+  "assets/image_02.png",
+  "assets/image_03.png",
+  "assets/image_04.png",
+];
+
+List<String> title = [
+  "Hounted Ground",
+  "Fallen in Love",
+  "The Dreaming Moon",
+  "Jack the Persian and the Black Castel",
+];
